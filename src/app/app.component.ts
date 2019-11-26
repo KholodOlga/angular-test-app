7d4d0121229bca12dfec29f4bc13b2cabd3c7d59
Kholod Olga <rukholod@gmail.com>
2019-11-27 02:01:18 +0300

    finish app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-test-app';
+
+  titleMovie: string;
+  movies: any[];
+  movie: any;
+  omdbapiUrl = `http://www.omdbapi.com/?apikey=${environment.imdbKey}`;
+
+  constructor(private http: HttpClient) {}
+
+  getFilms() {
+    this.http.get(`${this.omdbapiUrl}&s=${this.titleMovie}`).subscribe((data: any) => {
+      this.movies = data.Search;
+      this.movie = null;
+    })
+  }
+
+  openFilm(id) {
+    this.http.get(`${this.omdbapiUrl}&i=${id}`).subscribe((data: any) => {
+      this.movie = data;
+    })
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  imdbKey: '1fb96f1b'
 };
 
 /*
